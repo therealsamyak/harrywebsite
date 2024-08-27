c0882298001017407d192a12ff486eb381e968bf
alskaf1293 <timothyohoc@gmail.com>
2024-08-26 18:37:11 -0700

    version before experimenting with google drive api

--- src/components/ArtGallery.js
+++ src/components/ArtGallery.js
@@ -6,17 +6,6 @@ function ArtGallery() {
     <section className="section art-gallery">
       <h1>Art Gallery</h1>
 
-      {/* Responsive Embedded YouTube Video */}
-      <div className="video-container">
-        <iframe 
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
-          title="YouTube video player" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-          allowFullScreen
-        ></iframe>
-      </div>
-
       {/* Art */}
     </section>
   );
--- src/components/Films.js
+++ src/components/Films.js
@@ -4,7 +4,16 @@ function Films() {
   return (
     <section className="section films">
       <h1>Films</h1>
-      {/* Add content related to films here */}
+      {/* Responsive Embedded YouTube Video */}
+      <div className="video-container">
+        <iframe 
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+          title="YouTube video player" 
+          frameBorder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowFullScreen
+        ></iframe>
+      </div>
     </section>
   );
 }

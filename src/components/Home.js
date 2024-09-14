1debc7fdd232dd730e31b861fa4683b530712fe3
Samyak Kakatur <therealsamyak@gmail.com>
2024-09-14 11:31:15 -0700

    fixed home screen issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,29 @@
 // src/components/Home.js
 import React from 'react';
-import './Home.css'; // Custom CSS file for styling
-import HomeNavbar from './HomeNavbar'; // Importing HomeNavbar component
+import { Link } from 'react-router-dom';
+import './Home.css'; // Import the combined CSS for the Home component
 
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="title">WRATHFULPEST13</h1>
-      <div className="image-container">
-        <img src={process.env.PUBLIC_URL + '/images/homepage.png'} alt="Artistic Home" className="homepage-image" />
-      </div>
+      <h1 class="home-title">WRATHFULPEST13</h1>
       
-      {/* Replace the static text with HomeNavbar for "ART FILMS STORY ME" */}
-      <HomeNavbar />
+      <div className="image-container">
+        <img
+          src={process.env.PUBLIC_URL + '/images/homepage.png'}
+          alt="Artistic Home"
+          className="homepage-image"
+        />
+      </div>
 
+      <nav className="home-navbar">
+        <ul className="homenavbar-links">
+          <li><Link to="/art">ART</Link></li>
+          <li><Link to="/films">FILMS</Link></li>
+          <li><Link to="/story">STORY</Link></li>
+          <li><Link to="/me">ME</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }

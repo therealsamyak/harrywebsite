a22e8e4ef20c81123d4492239ea88c915687c28b
Samyak Kakatur <therealsamyak@gmail.com>
2024-09-14 10:33:41 -0700

    fixed test from default

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders website', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/wrathfulpest13/i);
+  expect(heading).toBeInTheDocument();
 });

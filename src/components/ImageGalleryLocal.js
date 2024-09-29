9c8bbe26e3729ca5240ea4180ce05ac0973472db
samheart564 <samheart564@gmail.com>
2024-09-29 14:49:15 -0700

    fixed regex

--- src/components/ImageGalleryLocal.js
+++ src/components/ImageGalleryLocal.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Use require.context to dynamically load all images from the src/art_gallery folder
 const importAll = (requireContext) => requireContext.keys().map(requireContext);
-const images = importAll(require.context('../art_gallery', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../art_gallery', false, /.*/));
 
 // Helper function to extract just the filename from the full path
 const getFileName = (path) => {
@@ -16,19 +16,19 @@ const sortedImages = images.sort((a, b) => {
   return fileNameA.localeCompare(fileNameB); // Compare filenames lexicographically
 });
 
-console.log(sortedImages)
+console.log(sortedImages);
 
 const ImageGalleryLocal = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-        {sortedImages.map((image, index) => (
-            <img
-            key={index}
-            src={image}
-            alt={`Art Gallery ${index + 1}`}
-            className="w-full lg:w-4/5 h-auto mb-4"
-            />
-        ))}
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      {sortedImages.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Art Gallery ${index + 1}`}
+          className='w-full lg:w-4/5 h-auto mb-4'
+        />
+      ))}
     </div>
   );
 };
